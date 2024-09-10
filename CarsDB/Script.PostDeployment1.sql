dcedb292e21f2781920673d0ec1ec1f8495a0d11
Raziel1991 <33969670+Raziel1991@users.noreply.github.com>
2024-09-10 10:10:34 -0400

    Added Values to table in a postdeployment script

---
+++ CarsDB/Script.PostDeployment1.sql
@@ -0,0 +1,15 @@
+﻿if not exists (select 1 from dbo.[Cars])
+begin 
+	insert into dbo.[Cars] (Make, Model, Year, Price)
+	values 
+	('Toyota', 'Camry', 2020, 24000.00),
+    ('Honda', 'Civic', 2019, 22000.00),
+    ('Ford', 'Mustang', 2021, 35000.00),
+    ('Chevrolet', 'Malibu', 2018, 20000.00),
+    ('Tesla', 'Model 3', 2022, 40000.00),
+    ('BMW', '3 Series', 2021, 41000.00), 
+    ('Audi', 'A4', 2020, 38000.00),
+    ('Mercedes', 'C-Class', 2019, 42000.00),
+    ('Hyundai', 'Elantra', 2021, 19000.00),
+    ('Kia', 'Optima', 2020, 21000.00);
+end
---
+++ CarsDB/dbo/StoredProceddures/spCars_Update.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[spCars_Update]
+	@Id INT,
+	@Make NVARCHAR (20),
+	@Model NVARCHAR (20),
+	@Year INT,
+	@Price DECIMAL (10,2)
+AS
+begin
+	update dbo.[Cars]
+	set Make = @Make, Model = @Model, Year = @Year, Price = @Price
+	where Id = @Id
+end 
